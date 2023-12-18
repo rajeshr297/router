fd384b6d021b66e475bbed44aa7129e6a389facb
Rajesh R <127257433+rajeshr297@users.noreply.github.com>
2023-12-18 14:44:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,3 @@
-// //   import logo from "./logo.svg";
-// import React from "react";
-// import "./App.css";
-// import Home from "./Components/Home.jsx";
-// import About from "./Components/About.jsx";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./Components/Navbar.jsx";
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar />
-//         <BrowserRouter>
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//           </Routes>
-//         </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
-// export default App;
-
 import React from "react";
 import "./App.css";
 import Home from "./Components/Home.jsx";
